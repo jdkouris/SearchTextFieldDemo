06b0bf97cf693b64f4ea187f93deffaa7feaae60
John Kouris <jdkouris@gmail.com>
2019-10-16 09:43:51 -0700

    Initial Commit

---
+++ SearchTextFieldDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SearchTextFieldDemo
+//
+//  Created by John Kouris on 10/16/19.
+//  Copyright © 2019 John Kouris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
